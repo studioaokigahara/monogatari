0183f5681e0915fb1afca1297a55ecd656b34081
a-wxve <117374906+a-wxve@users.noreply.github.com>
2025-05-22 05:02:15 -0500

    Remove service worker registration code

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,6 @@ import { router } from "@/router";
 import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import workerUrl from "./service-worker.ts?worker&url";
-
-let swPath: string = workerUrl;
-
-if ("serviceWorker" in navigator) {
-    // navigator.serviceWorker
-    //     .register("service-worker.ts", { type: "module" })
-    //     .then(() => console.log("Service Worker registered."));
-    navigator.serviceWorker.addEventListener("message", (evt) => {
-        if ((evt.data as any).__swLog) {
-            console.log("[from SW]", ...(evt.data as any).__swLog);
-        }
-    });
-}
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
