856f0ae0abdf54e5662dad27a3a1418bf15c7360
a-wxve <117374906+a-wxve@users.noreply.github.com>
2026-01-15 20:12:40 -0500

    move downloadFile and FNV1a to utils, add getFileExtension

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
+import { init } from "@paralleldrive/cuid2";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { init } from "@paralleldrive/cuid2";
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -12,4 +12,36 @@ export function debug(message: string, ...args: unknown[]): void {
     if (process.env.NODE_ENV === "development") {
         console.log(message, ...args);
     }
+}
+
+export function FNV1a(string: string): number {
+    let hash = 0x811c9dc5;
+    for (let i = 0; i < string.length; i++) {
+        hash ^= string.charCodeAt(i);
+        hash = Math.imul(hash, 0x01000193);
+    }
+    hash = hash >>> 0;
+    return hash / 0x100000000;
+}
+
+export function downloadFile(file: File) {
+    const url = URL.createObjectURL(file);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = file.name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+/**
+ * @author VisioN
+ * @link https://stackoverflow.com/questions/190852/how-can-i-get-file-extensions-with-javascript/12900504#12900504 
+ * @retrieved 2026-01-12
+ * @license CC BY-SA 4.0
+ */
+export function getFileExtension(filename: string) {
+    const extension = filename.slice(((filename.lastIndexOf(".") - 1) >>> 0) + 2);
+    return extension.trim() ? extension : "unknown";
 }
