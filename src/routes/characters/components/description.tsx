9e6ffc0bacac0092dac3a1717bdaf2cc95c7763a
a-wxve <117374906+a-wxve@users.noreply.github.com>
2026-01-15 19:59:36 -0500

    add sub-tabs to character description tab

--- src/routes/characters/components/description.tsx
+++ src/routes/characters/components/description.tsx
@@ -1,42 +1,78 @@
-import { Prose } from "@/components/prose";
-import { Card, CardContent } from "@/components/ui/card";
+import { Markdown } from "@/components/markdown";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Character } from "@/database/schema/character";
+import { cn } from "@/lib/utils";
 
 export function Description({ character }: { character: Character }) {
+    const showTabs =
+        character.data.personality ||
+        character.data.scenario ||
+        character.data.system_prompt ||
+        character.data.post_history_instructions;
     return (
         <Card>
-            <CardContent className="space-y-4">
-                <div>
+            <Tabs defaultValue="description">
+                <CardHeader className={cn(!showTabs && "hidden")}>
+                    <TabsList>
+                        <TabsTrigger value="description">
+                            Description
+                        </TabsTrigger>
+                        {character.data.personality && (
+                            <TabsTrigger value="personality">
+                                Personality
+                            </TabsTrigger>
+                        )}
+                        {character.data.scenario && (
+                            <TabsTrigger value="scenario">Scenario</TabsTrigger>
+                        )}
+                        {character.data.system_prompt && (
+                            <TabsTrigger value="system_prompt">
+                                System Prompt
+                            </TabsTrigger>
+                        )}
+                        {character.data.post_history_instructions && (
+                            <TabsTrigger value="post_history_instructions">
+                                Post-History Instructions
+                            </TabsTrigger>
+                        )}
+                    </TabsList>
+                </CardHeader>
+                <CardContent>
+                    <TabsContent value="description">
+                        <Markdown>{character.data.description}</Markdown>
+                    </TabsContent>
+                    {character.data.personality && (
+                        <TabsContent value="personality">
+                            <Markdown>{character.data.personality}</Markdown>
+                        </TabsContent>
+                    )}
                     {character.data.scenario && (
-                        <h4 className="font-medium mb-2">Description</h4>
+                        <TabsContent value="scenario">
+                            <Markdown>{character.data.scenario}</Markdown>
+                        </TabsContent>
                     )}
-                    <Prose>
-                        {character.data.description ??
-                            "No description provided."}
-                    </Prose>
-                </div>
-                {character.data.personality && (
-                    <div>
-                        <h4 className="font-medium mb-2">Personality</h4>
-                        <Prose className="max-w-[unset]">
-                            {character.data.personality}
-                        </Prose>
-                    </div>
-                )}
-                {character.data.scenario && (
-                    <div>
-                        <h4 className="font-medium mb-2">Scenario</h4>
-                        <Prose className="max-w-[unset]">
-                            {character.data.scenario}
-                        </Prose>
-                    </div>
-                )}
-                {!character.data.description &&
-                    !character.data.personality &&
-                    !character.data.scenario && (
-                        <Prose>Click edit to add character details...</Prose>
+                    {character.data.system_prompt && (
+                        <TabsContent value="system_prompt">
+                            <Markdown>{character.data.system_prompt}</Markdown>
+                        </TabsContent>
                     )}
-            </CardContent>
+                    {character.data.post_history_instructions && (
+                        <TabsContent value="post_history_instructions">
+                            <Markdown>
+                                {character.data.post_history_instructions}
+                            </Markdown>
+                        </TabsContent>
+                    )}
+                    {!character.data.description &&
+                        !character.data.personality &&
+                        !character.data.scenario && (
+                            <Markdown>
+                                Click edit to add character details...
+                            </Markdown>
+                        )}
+                </CardContent>
+            </Tabs>
         </Card>
     );
 }
