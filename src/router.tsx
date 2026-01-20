66f9bd8c0acbcab1a2fafebefad5cd71db103f0e
a-wxve <117374906+a-wxve@users.noreply.github.com>
2026-01-20 06:51:03 -0600

    delete explore route masks

--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,7 @@ import { Character } from "@/database/schema/character";
 import { queryClient } from "@/lib/query-client";
 import { routeTree } from "@/route-tree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouteMask, createRouter } from "@tanstack/react-router";
+import { createRouter } from "@tanstack/react-router";
 
 export interface Context {
     queryClient: QueryClient;
@@ -10,21 +10,8 @@ export interface Context {
     character?: Character;
 }
 
-const exploreChubMask = createRouteMask({
-    routeTree,
-    from: "/explore/chub",
-    to: "/explore"
-});
-
-const exploreAnchorholdMask = createRouteMask({
-    routeTree,
-    from: "/explore/anchorhold",
-    to: "/explore"
-});
-
 export const router = createRouter({
     routeTree,
-    routeMasks: [exploreChubMask, exploreAnchorholdMask],
     defaultPreload: "intent",
     defaultStructuralSharing: true,
     context: { queryClient },
